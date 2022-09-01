7c29bdb13517aa7a7263bc56a71400aa7f007177
Santosh Kumar <santrox444@gmail.com>
2022-09-01 19:14:02 +0400

    Added emois
    expanded menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title("My Parent's New Healthy Diner")
 streamlit.header("Menu")
 streamlit.text("Cabbage Fried in Olive Oil")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
